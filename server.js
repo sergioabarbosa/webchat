e527e0ade0f970011ff2ed1de81216eca5db262a
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2022-01-27 13:21:44 -0300

    Desafio 4

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const http = require('http').createServer(app);
 const socketIo = require('socket.io');
 const { format } = require('date-fns');
 const { insertMessage, getMessages } = require('./models/modelNewMessage');
-const error = require('./middlewares/error');
 const { createString } = require('./middlewares/helper');
 
 const io = socketIo(http, {
@@ -18,29 +17,43 @@ const io = socketIo(http, {
 
 app.use(cors());
 
-const listUser = [];
+const usersList = [];
 
+const dateTime = format(new Date(), 'dd-MM-yyy HH:mm:ss');
 io.on('connection', async (socket) => {
-    const messages = await getMessages();
+  const messages = await getMessages();
   socket.emit('historyMessages', messages);
-
   socket.on('newUser', (nickname) => {
-    listUser.push({ id: socket.id, nickname });
-    io.emit('newUser', listUser);
+    usersList.push({ id: socket.id, nickname });
+    io.emit('updateUsers', usersList);
   });
-    
-  const timestamp = format(new Date(), 'dd-MM-yyy HH:mm:ss');
 
   socket.on('message', async ({ chatMessage, nickname = createString(16) }) => {
-    io.emit('message', `${timestamp} - ${nickname} - ${chatMessage}`);
-    await insertMessage({ chatMessage, nickname, timestamp });
+    await insertMessage({ chatMessage, nickname, dateTime });
+    io.emit('message', `${dateTime} - ${nickname} - ${chatMessage}`);
+  });
+
+  socket.on('disconnect', async () => {
+    const users = usersList.findIndex((user) => user.id === socket.id);
+    usersList.splice(users, 1);
+    io.emit('updateUsers', usersList);
+  });
+});
+
+io.on('connection', async (socket) => {
+  socket.on('changeNick', (nickname) => {
+    usersList.forEach((user) => {
+      const userToEdit = user;
+      if (user.id === socket.id) {
+        userToEdit.nickname = nickname;
+      }
+    });
+    io.emit('updateUsers', usersList);
   });
 });
 
 app.get('/', (_req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
-
-app.use(error);
 
 http.listen(3000, () => console.log('Ouvindo a porta 3000'));
